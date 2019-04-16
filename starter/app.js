e44e2f25f11711be274502a6c5b4f4bc78787309
ashpremcode <ashpremecode@gmail.com>
2019-04-16 15:55:14 +1000

    added dice photo and reset scores to 0

--- starter/app.js
+++ starter/app.js
@@ -12,6 +12,24 @@ GAME RULES:
 var scores = [0, 0];
 var roundScores = 0;
 var activePlayer = 0;
-var dice = Math.floor(Math.random() * 6) + 1;
 
-console.log(dice);
+
+document.querySelector('.dice').style.display = 'none'; //hide the dice when page loads
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+	//1. Roll a random number.
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	//2. Display the result.
+	var diceDom = document.querySelector('.dice');
+	diceDom.style.display = 'block';
+	diceDom.src = 'dice-' + dice + '.png';
+
+
+});
+
+
